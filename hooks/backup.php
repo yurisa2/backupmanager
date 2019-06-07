044658af7f2b255cb58222044c2f838b84a9808d
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-07 20:34:28 -0300

    addmail

--- hooks/backup.php
+++ hooks/backup.php
@@ -1,9 +1,14 @@
 <?php
 ini_set("display_errors","on");
 include 'db_conn.php';
+include 'mailtemplate.php';
 
 $stmt = $dbh->prepare("INSERT INTO backups (label,date,obs,longdata,`update`,user) VALUES ('$_GET[label]',now(),'$_GET[obs]','$_GET[longdata]','$_GET[update]','$_GET[user]')");
 $stmt->execute();
+
+if(isset($_GET["email"]) {
+mail ($_GET["email"], "Backup " . $_GET["label"] . " feito.", corpo_mail($_GET["label"]));
+}
 
 var_dump($stmt); //DEBUG
 
