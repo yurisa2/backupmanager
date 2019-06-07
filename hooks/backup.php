a5a2531a15bd22bf8d066f456ddba318923a246c
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-07 19:54:53 -0300

    BackupUpdated

--- hooks/backup.php
+++ hooks/backup.php
@@ -1,6 +1,11 @@
 <?php
+ini_set("display_errors","on");
+include 'db_conn.php';
+
+$stmt = $dbh->prepare("INSERT INTO backups (label,date,obs,longdata,update,user) VALUES ('$_GET[label]',now(),'$_GET[obs]','$_GET[longdata]','$_GET[update]','$_GET[user]')");
+$stmt->execute();
+
+// var_dump($stmt); //DEBUG
 
 file_put_contents("backup.json",json_encode($_GET));
-
-
 ?>
--- hooks/ping.php
+++ hooks/ping.php
@@ -1,19 +1,13 @@
 <?php
 ini_set("display_errors","on");
-
 include 'db_conn.php';
-
-
-
 
 $stmt = $dbh->prepare("INSERT INTO pings (date,obs,user) VALUES (now(),'$_GET[action]','$_GET[user]')");
 $stmt->execute();
 
 
-var_dump($stmt);
+// var_dump($stmt); //DEBUG
 
 file_put_contents("ping.json",json_encode($_GET));
-
-
 
 ?>
