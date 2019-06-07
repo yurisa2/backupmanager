57da5b57854d8f418df804732ae60923bc2f6b22
yurisa2-i3wm <yuri@sa2.com.br>
2019-06-07 19:25:48 -0300

    hooks

---
+++ hooks/db_conn.php
@@ -0,0 +1,13 @@
+<?php
+ini_set("display_errors","on");
+
+$server = "backupmanager.mysql.dbaas.com.br";
+
+$u = "backupmanager";
+$p = "Fokalove99";
+
+$dbh = new PDO("mysql:host=$server;dbname=$u", $u, $p);
+
+
+
+?>
--- hooks/ping.php
+++ hooks/ping.php
@@ -1,5 +1,11 @@
 <?php
+include 'db_conn.php';
 
+
+
+
+$stmt = $pdo->prepare("INSERT INTO pings (date,obs,user) VALUES(now(),$_GET[action],$_GET[user])");
+$stmt->execute();
 
 file_put_contents("ping.json",json_encode($_GET));
 
